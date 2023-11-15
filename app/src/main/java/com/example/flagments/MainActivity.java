9aa1f756936af9b37b3ae92f75291b76700ca276
Uraimov Botirjon <uraimovbotirjon@Uraimovs-MacBook-Pro.local>
2023-11-15 22:56:30 +0900

    Feat: Hiding and showing Fragments

--- app/src/main/java/com/example/flagments/MainActivity.java
+++ app/src/main/java/com/example/flagments/MainActivity.java
@@ -15,9 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firstFragment = new FirstFragment();
-        secondFragment = new SecondFragment();
-
         Button button1 = findViewById(R.id.btn1);
         Button button2 = findViewById(R.id.btn2);
 
@@ -25,22 +22,60 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.fragment_container, firstFragment)
-                        .addToBackStack(null)
-                        .commit();
+                firstFragment = (FirstFragment) getSupportFragmentManager().findFragmentByTag("first_fragment");
+                if (firstFragment ==null) {
+                    firstFragment = new FirstFragment();
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .setReorderingAllowed(true)
+                            .add(R.id.fragment_container, firstFragment, "first_fragment")
+                            //.addToBackStack(null)
+                            .commit();
+                } else {
+                    if (secondFragment !=null){
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .hide(secondFragment)
+                                .show(firstFragment)
+                                .commit();
+                    } else {
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .show(firstFragment)
+                                .commit();
+                    }
+                }
             }
         }));
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                secondFragment = (SecondFragment) getSupportFragmentManager().findFragmentByTag("second_fragment");
+                if (secondFragment == null){
+                    secondFragment = new SecondFragment();
+
                 getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.fragment_container, secondFragment)
-                        .addToBackStack(null)
+                        .setReorderingAllowed(true)
+                        .add(R.id.fragment_container, secondFragment, "second_fragment")
+                        //.addToBackStack(null)
                         .commit();
+                } else {
+
+                    if (firstFragment !=null){
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .hide(firstFragment)
+                                .show(secondFragment)
+                                .commit();
+                    } else {
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .show(secondFragment)
+                                .commit();
+                    }
+                }
 
             }
         });
